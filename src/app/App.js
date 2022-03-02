cb07384d50179701031ba94fa2f6c7e3d6889ae1
robin <rfussell17@gmail.com>
2022-03-02 11:05:07 -0800

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+// import About from "../components/About";
+// import SignUp from "../components/SignUp";
+// import Articles from "../components/Articles";
+// import Article from "../components/Article";
+// import Categories from "../components/Categories";
+// import Author from "../components/Author";
+// import Profile from "../components/Profile";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        {/* Add Routes here! */}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
