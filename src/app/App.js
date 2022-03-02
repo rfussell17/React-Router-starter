85043d556c76a97901cf4da333373d27e0b9a8fb
robin <rfussell17@gmail.com>
2022-03-02 11:32:14 -0800

    add routes to about, sign-up, articles, article, categories, author, profile, in App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-// import About from "../components/About";
-// import SignUp from "../components/SignUp";
-// import Articles from "../components/Articles";
-// import Article from "../components/Article";
-// import Categories from "../components/Categories";
-// import Author from "../components/Author";
-// import Profile from "../components/Profile";
-import { BrowserRouter as Router } from "react-router-dom";
+import About from "../components/About";
+import SignUp from "../components/SignUp";
+import Articles from "../components/Articles";
+import Article from "../components/Article";
+import Categories from "../components/Categories";
+import Author from "../components/Author";
+import Profile from "../components/Profile";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
 
@@ -17,7 +17,27 @@ function App() {
     <Router>
       <Header />
       <main>
-        {/* Add Routes here! */}
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/sign-up">
+          <SignUp />
+        </Route>
+        <Route path="/articles">
+          <Articles />
+        </Route>
+        <Route path="/article">
+          <Article />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+        <Route path="/author">
+          <Author />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
       </main>
       <Footer />
     </Router>
